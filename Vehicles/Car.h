f9378898e8011d2c39f493557f37274dde348c7d
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-04 13:24:17 -0700

    going thru tutorial

---
+++ Vehicles/Car.h
@@ -0,0 +1,20 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Nathan Wainwright on 2018-08-04.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+
+
+@end
